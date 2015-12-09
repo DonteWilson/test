d17c994f1e221ddb35b028ceec85809eb74e0819
dwilsonaieyr1 <JittNA@aol.com>
2015-12-08 18:31:23 -0600

    FINAL
    UPDATE

--- Graphics/Vc.py
+++ Graphics/Vc.py
@@ -1,16 +1,27 @@
 import VecCal
 from VecCal import *
 
-a = Vector(1,2,3)
-b = Vector(3,2,1)
+a = Vector(4,3,1)
+b = Vector(3,4,5)
 c = Vector(0,0,0)
-
+print("Vector Math Library into Python\n")
 c = a + b
 print("Addition: ")
 c.stats()
-print("")
-
-print("Magnitude".mag())
 
 c = a - b
+print("Subtraction: ")
 c.stats()
+
+c = a * b
+print("Multiplication: ")
+c.stats()
+
+print("Magnitude: ",a.mag())
+
+
+print("Dot Product: ",a.dot(b)) #dot may be predefined somewhere and when we ran it as dotP then back as dot it refreshed need to look up
+
+
+print("Lerp: ")
+
--- Graphics/VecCal.py
+++ Graphics/VecCal.py
@@ -1,32 +1,48 @@
 import math
 from math import *
-class Vector(object):
-		def init(self,ax,ay,az):
+class Vector():
+		def __init__(self,ax,ay,az):
 				self.x = ax
 				self.y = ay
 				self.z = az
+				
 		
 		def stats(self):
 			print(self.x,",",self.y,",",self.z)
 		
-		def add(self, other):
+		def __add__(self, other):
 				result = Vector(0,0,0)
 				result.x = self.x + other.x
 				result.y = self.y + other.y
 				result.z = self.z + other.z
 				return result
 			
-		def sub(self, other):
+		def __sub__(self, other):
 				result = Vector(0,0,0)
 				result.x = self.x - other.x
 				result.y = self.y - other.y
 				result.z = self.z - other.z
 				return result
+		
+		def __mul__(self, other):
+				result = Vector(0,0,0)
+				result.x = self.x * other.x
+				result.y = self.y * other.y
+				result.z = self.z * other.z
+				return result
 			
-		def mag(self, other):
+		def mag(self):
 				result = Vector(0,0,0)
 				result.x = self.x ^ 2
 				result.y = self.y ^ 2
 				result.z = self.z ^ 2
 				nresult = sqrt(result.x + result.y + result.z)
+				return nresult
+
+		def dot(self,other):
+				result = Vector(0,0,0)
+				result.x = self.x * other.x
+				result.y = self.y * other.y
+				result.z = self.z * other.z
+				nresult = (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
 				return nresult
