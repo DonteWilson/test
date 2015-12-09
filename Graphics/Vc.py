3a0e2cf19510bf55fdc7ceca24668cdbaa437847
dwilsonaieyr1 <JittNA@aol.com>
2015-12-09 13:08:15 -0600

    final
    final

--- Graphics/Vc.py
+++ Graphics/Vc.py
@@ -1,27 +1,106 @@
 import VecCal
 from VecCal import *
+#Vector Math Library in Python
+print("\nVector Math Library into Python\n")
 
-a = Vector(4,3,1)
-b = Vector(3,4,5)
-c = Vector(0,0,0)
-print("Vector Math Library into Python\n")
+a = Vector2(5,3)
+b = Vector2(4,10)
+c = Vector2(0,0)
+
+#Displays what is being printed
+print("Vector 2 consist of (5,3) and (4,7)")
+
+print(" ")
+
 c = a + b
+#Addition for Vector2
 print("Addition: ")
 c.stats()
 
 c = a - b
+#Subtraction for Vector2
 print("Subtraction: ")
 c.stats()
 
 c = a * b
+#Multiplication for Vector2
 print("Multiplication: ")
 c.stats()
+#Magnitudes for Vector 1 and 2
+print("Mag Vec1: ",a.mag())
+print("Mag Vec2: ",b.mag())
 
+print("Normalize: ")
+a.norm().stats()
+
+print("Normalize: ")
+b.norm().stats()
+#Dot Product for Vector 1 and 2
+print("Dot Product: ",a.dot(a))
+print("Dot Product: ",b.dot(b))
+#Linear Interpolation for Vector2
+print("Lerp:")
+result = a.lerp(b,.5)
+result.stats()
+
+a = Vector3(4,3,1)
+b = Vector3(3,4,5)
+c = Vector3(0,0,0)
+
+
+#Vector3 Information
+print("\nVector 3 consist of (4,3,1) and (3,4,5)\n")
+c = a + b
+#Addition for Vector3
+print("Addition: ")
+c.stats()
+
+c = a - b
+#Subtraction for Vector3
+print("Subtraction: ")
+c.stats()
+
+c = a * b
+#Multiplication for Vector3
+print("Multiplication: ")
+c.stats()
+#Cross Product for Vector 3
+print("Cross Product: ")
+result = a.cross(b)
+result.stats()
+
+#Magnitude for Vector3
 print("Magnitude: ",a.mag())
+print("Magnitude: ",b.mag())
 
+print("Normalize: ")
+a.norm().stats()
 
-print("Dot Product: ",a.dot(b)) #dot may be predefined somewhere and when we ran it as dotP then back as dot it refreshed need to look up
+print("Normalize: ")
+b.norm().stats()
 
+#Dot Product for Vector3
+print("Dot Product: ",a.dot(b))
 
+print("Dot Product: ",b.dot(b))#dot may be predefined somewhere and when we ran it as dotP then back as dot it refreshed need to look up
+
+#Lerp for Vector3
 print("Lerp: ")
+result = a.lerp(b,.5)
+result.stats()
+a = Vector4(225,0,0,0)
+print("\nVector 4\n")
+
+print("Normalize: ")
+a.norm().stats()
+
+print("Hex: 225,0,0,0")
+print("Hex: 225,64, 0, 0")
+print("Hex: 0,127,225")
+print hex(225),hex(0),hex(0),hex(0)
+print hex(225),hex(64),hex(0),hex(0)
+print hex(0),hex(127),hex(225)
+
+
+
 
--- Graphics/VecCal.py
+++ Graphics/VecCal.py
@@ -1,6 +1,62 @@
 import math
 from math import *
-class Vector():
+class Vector2(object):
+		def __init__(self,ax,ay):
+				self.x = ax
+				self.y = ay
+				
+		def stats(self):
+			print(self.x,self.y)		
+		
+		def __add__(self, other):
+				result = Vector2(0,0)
+				result.x = self.x + other.x
+				result.y = self.y + other.y
+				return result
+			
+		def __sub__(self, other):
+				result = Vector2(0,0)
+				result.x = self.x - other.x
+				result.y = self.y - other.y
+				return result
+		
+		def __mul__(self, other):
+				result = Vector2(0,0)
+				result.x = self.x * other.x
+				result.y = self.y * other.y
+				return result
+			    
+		def mag(self):
+				result = Vector2(0,0)
+				nresult = sqrt((self.x * self.x) + (self.y * self.y))
+				return nresult
+				
+		def norm(self):
+				result = Vector2(0,0)
+				mresult = sqrt((self.x * self.x) + (self.y * self.y))
+				result.x = self.x/mresult
+				result.y = self.y/mresult
+				return result
+				
+		def dot(self,other):
+				result = Vector2(0,0)
+				result.x = self.x * other.x
+				result.y = self.y * other.y
+				nresult = (self.x * other.x) + (self.y * other.y)
+				return nresult
+				
+		def lerp(self, other, per):
+				result = Vector2(0,0)
+				result.x = self.x + (per * (other.x - self.x))
+				result.y = self.y + (per * (other.y - self.y))
+				return result
+			    
+
+				
+				
+
+
+class Vector3(object):
 		def __init__(self,ax,ay,az):
 				self.x = ax
 				self.y = ay
@@ -8,41 +64,94 @@ class Vector():
 				
 		
 		def stats(self):
-			print(self.x,",",self.y,",",self.z)
+			print(self.x,self.y,self.z)
 		
 		def __add__(self, other):
-				result = Vector(0,0,0)
+				result = Vector3(0,0,0)
 				result.x = self.x + other.x
 				result.y = self.y + other.y
 				result.z = self.z + other.z
 				return result
 			
 		def __sub__(self, other):
-				result = Vector(0,0,0)
+				result = Vector3(0,0,0)
 				result.x = self.x - other.x
 				result.y = self.y - other.y
 				result.z = self.z - other.z
 				return result
 		
 		def __mul__(self, other):
-				result = Vector(0,0,0)
+				result = Vector3(0,0,0)
 				result.x = self.x * other.x
 				result.y = self.y * other.y
 				result.z = self.z * other.z
 				return result
-			
+		
+		def cross(self,other):
+				result = Vector3(0,0,0)
+				result.x = (self.y * other.z) - (other.y + self.z)
+				result.y = (self.z * other.x) - (other.z + self.x)
+				result.z = (self.x * other.y) - (other.x + self.y)
+				return result
+				
 		def mag(self):
-				result = Vector(0,0,0)
-				result.x = self.x ^ 2
-				result.y = self.y ^ 2
-				result.z = self.z ^ 2
-				nresult = sqrt(result.x + result.y + result.z)
+				result = Vector3(0,0,0)
+				nresult = sqrt((self.x * self.x) + (self.y * self.y) + (self.z * self.z))
 				return nresult
+				
+		def norm(self):
+				result = Vector3(0,0,0)
+				mresult = sqrt((self.x * self.x) + (self.y * self.y))
+				result.x = self.x/mresult
+				result.y = self.y/mresult
+				result.z = self.z/mresult
+				return result
 
 		def dot(self,other):
-				result = Vector(0,0,0)
+				result = Vector3(0,0,0)
 				result.x = self.x * other.x
 				result.y = self.y * other.y
 				result.z = self.z * other.z
 				nresult = (self.x * other.x) + (self.y * other.y) + (self.z * other.z)
 				return nresult
+				
+		def lerp(self, other, per):
+				result = Vector3(0,0,0)
+				result.x = self.x + (per * (other.x - self.x))
+				result.y = self.y + (per * (other.y - self.y))
+				result.z = self.z + (per * (other.z - self.z))
+				return result
+
+class Vector4(object):
+		def __init__(self,ar,ag,ab,aa):
+				self.r = ar
+				self.g = ag
+				self.b = ab
+				self.a = aa
+				
+		def stats(self):
+			print(self.r,self.g,self.b,self.a)
+				
+		def mag(self):
+				result = Vector4(0,0,0,0)
+				nresult = sqrt((self.r * self.r) + (self.g * self.g) + (self.b * self.b) + (self.a * self.a))
+				return nresult
+				
+		def norm(self):
+				result = Vector4(0,0,0,0)
+				mresult = sqrt((self.r * self.r) + (self.g * self.g) + (self.b * self.b) + (self.a * self.a))
+				result.r = self.r/mresult
+				result.g = self.g/mresult
+				result.b = self.b/mresult
+				result.a = self.a/mresult
+				return result
+		
+		def hex():
+				
+				hexc = (255,'#04X')
+				return hexc
+			
+				
+		
+		
+
